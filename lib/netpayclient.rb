5563f0d9358d9f17ddd4e313f76fb103a15ed6a4
Weirong Xu <weirongxu.raidou@gmail.com>
2016-07-28 16:40:56 +0800

    fix self method

--- lib/netpayclient.rb
+++ lib/netpayclient.rb
@@ -31,72 +31,74 @@ module Netpayclient
     Netpayclient.new(path: path, hash: hash)
   end
 
-  def hex2bin(hexdata)
-    [hexdata].pack "H*"
-  end
-
-  def padstr(src, len=256, chr='0', d='L')
-    src.strip!
-    case d
-    when 'L'
-      src.rjust(len, chr)
-    else
-      src.ljust(len, chr)
-    end
-  end
-
-  def bin2int(bindata)
-    bchexdec(bindata.unpack('H*')[0])
-  end
-
-  def bchexdec(hexdata)
-    hexdata.to_i(16)
-  end
-
-  def bcdechex(decdata)
-    decdata.to_s(16)
-  end
-
-  def sha1_128(string)
-    require 'digest/sha1'
-    hash = Digest::SHA1.hexdigest(string)
-    sha_bin = hex2bin(hash)
-    sha_pad = hex2bin(HASH_PAD)
-    sha_pad + sha_bin
-  end
-
-  def mybcpowmod(num, pow, mod)
-    num.to_bn.mod_exp(pow, mod)
-  end
-
-  def rsa_encrypt(private_key, input)
-    p = bin2int(private_key[:prime1])
-    q = bin2int(private_key[:prime2])
-    u = bin2int(private_key[:coefficient])
-    dP = bin2int(private_key[:prime_exponent1])
-    dQ = bin2int(private_key[:prime_exponent2])
-    c = bin2int(input)
-    cp = c % p
-    cq = c % q
-    a = mybcpowmod(cp, dP, p)
-    b = mybcpowmod(cq, dQ, q)
-    if a > b
-      result = a - b
-    else
-      result = b - a
-      result = p - result
-    end
-    result = result % p
-    result = result * u
-    result = result % p
-    result = result * q
-    result = result + b
-    ret = bcdechex(result)
-    ret = padstr(ret).upcase
-    ret.size == 256 ? ret : false
-  end
-
   class Netpayclient
+    class << self
+      def hex2bin(hexdata)
+        [hexdata].pack "H*"
+      end
+
+      def padstr(src, len=256, chr='0', d='L')
+        src.strip!
+        case d
+        when 'L'
+          src.rjust(len, chr)
+        else
+          src.ljust(len, chr)
+        end
+      end
+
+      def bin2int(bindata)
+        bchexdec(bindata.unpack('H*')[0])
+      end
+
+      def bchexdec(hexdata)
+        hexdata.to_i(16)
+      end
+
+      def bcdechex(decdata)
+        decdata.to_s(16)
+      end
+
+      def sha1_128(string)
+        require 'digest/sha1'
+        hash = Digest::SHA1.hexdigest(string)
+        sha_bin = hex2bin(hash)
+        sha_pad = hex2bin(HASH_PAD)
+        sha_pad + sha_bin
+      end
+
+      def mybcpowmod(num, pow, mod)
+        num.to_bn.mod_exp(pow, mod)
+      end
+
+      def rsa_encrypt(private_key, input)
+        p = bin2int(private_key[:prime1])
+        q = bin2int(private_key[:prime2])
+        u = bin2int(private_key[:coefficient])
+        dP = bin2int(private_key[:prime_exponent1])
+        dQ = bin2int(private_key[:prime_exponent2])
+        c = bin2int(input)
+        cp = c % p
+        cq = c % q
+        a = mybcpowmod(cp, dP, p)
+        b = mybcpowmod(cq, dQ, q)
+        if a > b
+          result = a - b
+        else
+          result = b - a
+          result = p - result
+        end
+        result = result % p
+        result = result * u
+        result = result % p
+        result = result * q
+        result = result + b
+        ret = bcdechex(result)
+        ret = padstr(ret).upcase
+        ret.size == 256 ? ret : false
+      end
+    end
+
     def initialize(path: nil, hash: {})
       require 'iniparse'
       @private_key = {}
@@ -117,7 +119,7 @@ module Netpayclient
       else
         raise 'config error'
       end
-      bin = hex2bin(hex)
+      bin = self.class.hex2bin(hex)
       @private_key[:modulus] = bin[0,128]
 
       prime1 = bin[384,64]
@@ -138,20 +140,20 @@ module Netpayclient
     end
 
     def rsa_decrypt(input)
-      check = bchexdec(input)
-      modulus = bin2int(@private_key[:modulus])
-      exponent = bchexdec("010001")
-      result = mybcpowmod(check, exponent, modulus)
-      rb = bcdechex(result)
-      padstr(rb).upcase
+      check = self.class.bchexdec(input)
+      modulus = self.class.bin2int(@private_key[:modulus])
+      exponent = self.class.bchexdec("010001")
+      result = self.class.mybcpowmod(check, exponent, modulus)
+      rb = self.class.bcdechex(result)
+      self.class.padstr(rb).upcase
     end
 
     def sign(msg)
       if not @private_key.key?(:MERID)
         return false
       end
-      hb = sha1_128(msg)
-      return rsa_encrypt(@private_key, hb)
+      hb = self.class.sha1_128(msg)
+      return self.class.rsa_encrypt(@private_key, hb)
     end
 
     def sign_order(merid, ordno, amount, curyid, transdate, transtype)
@@ -168,7 +170,7 @@ module Netpayclient
     def verify(plain, check)
       return false if not @private_key.key?(:PGID)
       return false if check.size != 256
-      hb = sha1_128(plain)
+      hb = self.class.sha1_128(plain)
       hbhex = hb.unpack('H*')[0].upcase
       rbhex = rsa_decrypt(check)
       return hbhex == rbhex ? true : false
